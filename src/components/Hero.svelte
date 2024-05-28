ca8e1a3d458cbbd4fb658b31d27c75c007e6a8ac
valivia <26413018+valivia@users.noreply.github.com>
2024-05-28 11:57:36 +0000

    🦉 initial commit

---
+++ src/components/Hero.svelte
@@ -0,0 +1,23 @@
+<header>
+    <h1>Hootsifer</h1>
+    <p>A multi-purpose Discord bot</p>
+    <!-- <p>Trusted by <b>{props.status.memberCount}</b> members</p>
+    <p>In <b>{props.status.guildCount}</b> communities</p> -->
+</header>
+
+<style lang="scss">
+    header {
+        & h1 {
+            margin-bottom: 6px;
+        }
+
+        & p {
+            padding: 0;
+            margin: 5px 0;
+        }
+
+        * b {
+            color: var(--theme-accent);
+        }
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+{@render children()}
+
+<svelte:head>
+    <meta name="theme-color" content="#bf8d50" />
+</svelte:head>
+
+<style lang="scss" global>
+    @import "styles/global.scss";
+</style>
